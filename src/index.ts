a659f8eed951bfff84f621403dd75df908ce3f12
tmhuyett <toddhuyett@gmail.com>
2021-12-18 02:16:22 -0500

    'd'

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,50 @@ const path = require("path")
 
 const app: Application = express()
 const port: number = 3000
-app.use(express.static(path.join(__dirname, "..", "build")))
-app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", async (req: Request, res: Response): Promise<any> => {
-  res.status(200).sendFile(path.join(__dirname, "..", "build", "index.html"))
+  res.status(200).send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <style>
+          html {
+              background-color: rgb(210, 215, 240);
+          }
+        p {
+          font-family: Helvetica;
+        }
+        #code {
+          font-family: courier;
+          background-color: rgb(226, 108, 108);
+        }
+        .centered {
+          position: fixed;
+          top: 50%;
+          left: 50%;
+          width: 90%;
+          max-width: 900px;
+          transform: translate(-50%, -50%);
+        }
+      </style>
+    </head>
+    <body>
+      <div>
+        <div class="centered">
+          <p>
+            This backend is powered by Stripe, Ubuntu, AWS EC2, Docker, Nginx,
+            Typescript, and Node/Express. It exposes single hard-coded
+            <span id="code">.post()</span> endpoint for
+            <a href="https://suspicious-jepsen-411cb1.netlify.app/"
+              >this product</a
+            >. The code can be found <a href="https://github.com/prokopious/docker">here</a>.
+          </p>
+        </div>
+      </div>
+    </body>
+  </html>
+  `)
 })
 
 const YOUR_DOMAIN: string = "https://suspicious-jepsen-411cb1.netlify.app/"
